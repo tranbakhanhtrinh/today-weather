a35be8a3eaa6899a4455ef78595076c2a1454528
tranbakhanhtrinh <tranbakhanhtrinh@gmail.com>
2023-10-19 01:36:08 +0700

    responsive

--- components/SearchHistory/index.tsx
+++ components/SearchHistory/index.tsx
@@ -3,7 +3,7 @@ import SearchRecord from '@/components/SearchHistory/SearchRecord'
 
 const SearchHistory = () => {
   return (
-    <div className='bg-white h-full sm:h-[400px] overflow-y-scroll pt-4'>
+    <div className='bg-white h-[400px] w-full md:w-[612px] rounded mx-auto overflow-y-scroll pt-4'>
       <h1 className='text-2xl pb-4 font-bold px-4'>Search History</h1>
       <hr className='mb-4' />
       <SearchRecord />
